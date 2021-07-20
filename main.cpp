7955a8af1291a919d39dba3e5b651f44af1593fd
“RS <rohini.rs@ltts.com>
2021-07-20 19:29:05 +0530

    Second commit

--- main.cpp
+++ main.cpp
@@ -15,4 +15,6 @@ int main()
     std::cout<<"\nTriangle area: "<<tri_ptr->area();
     IShape *circle_ptr = &circle;
     std::cout<<"\nCircle circumference: "<<circle_ptr->circumference();
+   std::cout<<"\nNew branch";
+   
 }
